ff0538244a5de860c2d6d73ba2418705a702576c
DAVID DE SOUZA CARDOSO <RM76860@fiap.com.br>
2017-02-16 21:57:02 -0200

    Initial commit

---
+++ Tabuada/app/src/main/java/com/davidenterprises/tabuada/TabuadaActivity.java
@@ -0,0 +1,13 @@
+package com.davidenterprises.tabuada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabuadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabuada);
+    }
+}
